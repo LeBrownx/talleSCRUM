6c7ca96b55578d7aa302ee90dd2bbe01b0f4cada
KevinAltamirano <kevinaltperez@gmail.com>
2016-07-28 11:43:32 -0500

    Politicas
    Formulario & Envio de Datos

---
+++ Politicas/politicasFormulario.php
@@ -0,0 +1,41 @@
+<?php
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>POLITICAS</title>
+  </head>
+  <body>
+    <h2>Elaboracion de Politicas</h2>
+    <form class="" action="guardarPoliticas.php" method="post">
+      <input type="text" name="materia" value="" placeholder="Materia">
+      <p>
+        Cuatrimestre
+      </p>
+      <select name="cuatrimestre">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+      </select>
+      <br>
+      <p>
+        Politicas
+      </p>
+      <textarea name="politicas" rows="8" cols="40" placeholder="Politicas..."></textarea>
+      <input type="submit" name="name" value="Guardar">
+    </form>
+
+
+  </body>
+</html>
