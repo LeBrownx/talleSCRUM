4dc7d4a2a53a4cf82b555635b6c43856bc91ca27
KevinAltamirano <kevinaltperez@gmail.com>
2016-07-28 13:28:19 -0500

    Validacion Terminada
    Validacion y PHP completos

--- registroUsuario.php
+++ registroUsuario.php
@@ -5,16 +5,30 @@ if(!empty($_POST['nombre'])){
         $pass = $_POST['pass'];
         $email = $_POST['email'];
         $tipoUsuario = $_POST['TipoUsuario'];
-
+        $cuatrimestre = $_POST['cuatrimestre'];
         mysql_connect('localhost','root','root')
         or die("Error al conectar " . mysql_error());
         mysql_select_db('taller_scrum') //aca era proyecto
         or die ("Error al seleccionar la Base de datos: " . mysql_error());
-        if(tipoUsuario == "PTC"){
-            mysql_query();
-            $informacion = mysql_query("INSERT INTO ptc");
+        if($tipoUsuario == "PTC"){
+            mysql_query("INSERT INTO usuario (nombre,apellidos,contrasena,correo) VALUES('$nombre', '$apellidos', '$pass', '$email')");
+            $id = mysql_query("SELECT LAST_INSERT_ID()");
+            $idUsuario = mysql_fetch_array($id);
+            $clave = 1000;
+            $c = mysql_query("SELECT  COUNT(*) from ptc");
+            $count = mysql_fetch_array($c);
+            $clave = $clave + $count[0];
+            //echo $idUsuario[0];
+            mysql_query("INSERT INTO ptc(idUsuario, clave) VALUES('$idUsuario[0]', '$clave')");
         }else{
-
+            mysql_query("INSERT INTO usuario (nombre,apellidos,contrasena,correo) VALUES('$nombre', '$apellidos', '$pass', '$email')");
+            $id = mysql_query("SELECT LAST_INSERT_ID()");
+            $idUsuario = mysql_fetch_array($id);
+            $matricula = 14000;
+            $c = mysql_query("SELECT  COUNT(*) from alumno");
+            $count = mysql_fetch_array($c);
+            $matricula= $matricula + $count[0];
+            mysql_query("INSERT INTO alumno (idUsuario, matricula, cuatrimestre) VALUES('$idUsuario[0]', '$matricula', '$cuatrimestre')");
         }
     }
 ?>
@@ -25,13 +39,13 @@ if(!empty($_POST['nombre'])){
         <script type="text/javascript">
         function habilitar(value)
         {
-          if(value=="PTC" || value==true)
+          if(value=="Alumno" || value==true)
           {
             // habilitamos
-            document.getElementById("cuatri").disabled=false;
-          }else if(value=="Alumno" || value==false){
+            document.getElementById("cuatri").hidden=false;
+          }else if(value=="PTC" || value==false){
             // deshabilitamos
-            document.getElementById("cuatri").disabled=true;
+            document.getElementById("cuatri").hidden=true;
           }
         }
         </script>
@@ -42,9 +56,10 @@ if(!empty($_POST['nombre'])){
             <input type= "text" placeholder="Apellidos" name="apellidos">
             <input type= "password" placeholder="Contraseña" name="pass">
             <input type= "email" placeholder="Correo" name="email">
-            <select name="TipoUsuario" id="usuarioT" onchange="habilitar()">
-                <option value="PTC">PTC</option>
+            <select name="TipoUsuario" id="usuarioT" onchange="habilitar(this.value);">
                 <option value="Alumno">Alumno</option>
+                <option value="PTC">PTC</option>
+
             </select>
             <select name="cuatrimestre" id="cuatri">
                 <option value="1">1</option>
